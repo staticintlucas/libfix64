8db9e776414962af7b0e84dcbc0ba3616eaab5e4
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2022-09-24 17:37:23 +0100

    Fix test not running correctly for all trig functions

--- test/test_trig.py
+++ test/test_trig.py
@@ -2,9 +2,8 @@ import math
 
 from . import *
 
-angles = fix_range(-2 * math.pi, 2 * math.pi, 1e5)
-
 def test_sin(libfix64):
+    angles = fix_range(-2 * math.pi, 2 * math.pi, 1e5)
     for angle in angles:
         expected = math.sin(fix2flt(angle))
         result = libfix64.fix64_sin(angle)
@@ -12,6 +11,7 @@ def test_sin(libfix64):
         assert isclose(fix2flt(result), expected), f"angle = {angle}"
 
 def test_cos(libfix64):
+    angles = fix_range(-2 * math.pi, 2 * math.pi, 1e5)
     for angle in angles:
         expected = math.cos(fix2flt(angle))
         result = libfix64.fix64_cos(angle)
@@ -19,6 +19,7 @@ def test_cos(libfix64):
         assert isclose(fix2flt(result), expected), f"angle = {angle}"
 
 def test_tan(libfix64):
+    angles = fix_range(-2 * math.pi, 2 * math.pi, 1e5)
     for angle in angles:
         expected = sat_flt(math.tan(fix2flt(angle)))
         result = libfix64.fix64_tan(angle)
