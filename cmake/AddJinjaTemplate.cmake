0e9ce37812e727839155da10fd740958a483aa6a
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2022-10-29 22:59:10 +0100

    Move Jinja rendering to separate CMake module

---
+++ cmake/AddJinjaTemplate.cmake
@@ -0,0 +1,40 @@
+# add_jinja_template
+#
+# Add a Jinja2 template to be rendered
+#
+# Usage
+# add_jinja_template(
+#     TEMPLATES <file>...
+#     OUTPUT_SOURCES <variable>
+# )
+function(add_jinja_template)
+    cmake_parse_arguments(PARSE_ARGV 0 ARG "" "OUTPUT_SOURCES" "TEMPLATES")
+
+    if(DEFINED ARG_UNPARSED_ARGUMENTS)
+        list_index(FIRST_UNPARSED 0 ${ARG_UNPARSED_ARGUMENTS})
+        message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} called with unknown argument \"${FIRST_UNPARSED}\".")
+    endif()
+    foreach(ARG IN ITEMS TEMPLATES OUTPUT_SOURCES)
+        if(NOT DEFINED ARG_${ARG})
+            message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION} called with no value for ${ARG}.")
+        endif()
+    endforeach()
+
+    set(GENERATED_SOURCES "")
+
+    foreach(SRC ${ARG_TEMPLATES})
+        # Add build command
+        add_custom_command(
+            OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${SRC}"
+            COMMAND ${Python3_EXECUTABLE} jinja.py
+                "${CMAKE_CURRENT_SOURCE_DIR}/${SRC}.jinja" "${CMAKE_CURRENT_BINARY_DIR}/${SRC}"
+            WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/scripts"
+            DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/${SRC}.jinja"
+        )
+
+        # Append generated file to list
+        list(APPEND GENERATED_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/${SRC}")
+    endforeach()
+
+    set(${ARG_OUTPUT_SOURCES} ${GENERATED_SOURCES} PARENT_SCOPE)
+endfunction()
