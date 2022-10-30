c9ba28c0eb7090bf328e40d0086e4d04cf02c6fa
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2022-10-30 16:04:14 +0000

    Enable formatting of .inc files

--- scripts/jinja.py
+++ scripts/jinja.py
@@ -76,7 +76,7 @@ def render(filename):
         undefined=_jinja2.StrictUndefined)
     template = env.get_template(filename.name)
 
-    return template.render(args)
+    return template.render(args).rstrip("\n")
 
 
 if __name__ == "__main__":
