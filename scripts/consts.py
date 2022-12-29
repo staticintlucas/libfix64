561d0a39daed9b573a803903e0363b74a692c2cc
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2022-12-29 01:51:13 +0000

    use mpmath.chebyfit to calculate chebyshev polynomial coefficients

--- scripts/consts.py
+++ scripts/consts.py
@@ -1,7 +1,4 @@
 from mpmath import mp as _mp, mpf as _mpf
-import numpy as _np
-from numpy.polynomial.chebyshev import cheb2poly as _cheb2poly
-from numpy.polynomial import Chebyshev as _Cheby
 
 _mp.prec = 256
 
@@ -27,17 +24,7 @@ sqrt1_2 = sqrt2 / 2
 def chebyshev_coefs(func, n_coef):
 
     def impl():
-        mpcos = _np.vectorize(_mp.cos)
-        vfunc = _np.vectorize(func)
-
-        N = _mpf(n_coef)
-        k = _np.arange(N, dtype=_mpf)
-        n = _np.arange(N, dtype=_mpf)
-
-        xk = mpcos(pi * (k + half) / N)
-        an = (((two - (n == 0)) / N) *
-            _np.sum(mpcos(_np.outer(n * pi, k + half) / N) * vfunc(xk), axis=1))
-
-        return _cheb2poly(an)[::-1] # reverse to make C implementation slightly simpler
+        an = _mp.chebyfit(func, [-1, 1], n_coef)
+        return an # reverse to make C implementation slightly simpler
 
     return impl
