0ff45549d94f8760990f1bb1c0f40d9d38e48319
Lucas Jansen <7199136+staticintlucas@users.noreply.github.com>
2023-01-01 02:37:14 +0000

    Reduce trig function footprint and number of branches

--- src/math/trig.c
+++ src/math/trig.c
@@ -6,42 +6,36 @@
 #include "math/trig.inc"
 
 fix64_t fix64_sin(fix64_t angle) {
-    // Normalise so that 1.0 = pi/2 = 90deg
+    // Normalise so that 1.0 = pi/4 = 45deg
     int64_t angle_hi;
-    uint64_t angle_lo = fix64_impl_mul_i64_i128(angle.repr, CHEBYSHEV_2_PI, &angle_hi); // Q31.94
+    uint64_t angle_lo = fix64_impl_mul_i64_i128(angle.repr, CHEBYSHEV_4_PI, &angle_hi); // Q32.94
 
-    // Calculate octant, used to determine which chebyshev function to call
-    uint32_t hi_frac_bits = CHEBYSHEV_FRAC_BITS + FIX64_FRAC_BITS - 64; // frac bits in angle_hi
-    unsigned octant = (angle_hi >> (hi_frac_bits - 1)) & 0x7;
-    // Modulo pi/2 (90deg)
-    angle_hi &= (1ull << hi_frac_bits) - 1; // UQ0.94
+    // Modulo 8 (2pi, i.e. 360deg)
+    unsigned hi_frac_bits = CHEBYSHEV_FRAC_BITS + FIX64_FRAC_BITS - 64; // frac bits in angle_hi
+    angle_hi &= (1ull << (hi_frac_bits + 3)) - 1; // UQ3.94
 
-    // Normalise -pi/4..pi/4 (90deg) to range -1..1 with CHEBYSHEV_FRAC_BITS fractional bits
+    // Normalise to Q3.62, so angle is in the range [-pi/4, pi/4)
+    angle_lo =
+        ((uint64_t)angle_hi << (64 - FIX64_FRAC_BITS)) | (angle_lo >> FIX64_FRAC_BITS); // UQ3.62
+    // Constrain to range -1..1 (i.e. -pi/4..pi/4)
+    angle_lo =
+        ((angle_lo + CHEBYSHEV_ONE) & (CHEBYSHEV_TWO - 1)) - CHEBYSHEV_ONE; // Q2.62 as unsigned
+    // Avoid UB of unsigned -> signed conversion overflow
     int64_t norm_a =
-        (angle_hi << (64 - (FIX64_FRAC_BITS - 1))) | (angle_lo >> (FIX64_FRAC_BITS - 1)); // Q1.62
-    norm_a =
-        (((norm_a + (1ull << CHEBYSHEV_FRAC_BITS)) & ((2ull << CHEBYSHEV_FRAC_BITS) - 1)) -
-         (1ull << CHEBYSHEV_FRAC_BITS)); // Q1.62
+        (angle_lo > INT64_MAX) ? (int64_t)(angle_lo - INT64_MIN) + INT64_MIN : (int64_t)angle_lo;
+    // Which eighth of the unit circle the angle lies in
+    unsigned octant = (angle_hi >> hi_frac_bits) + 1; // 1-8
+
+    int neg_result = (octant & 4) != 0; // negate result for 4,5,6,7
+    int use_cos = (octant & 2) != 0; // use cos for 2,3,6,7
 
     int64_t result = 0;
-    switch (octant) {
-        case 7:
-        case 0:
-            result = chebyshev_sin_impl(norm_a);
-            break;
-        case 1:
-        case 2:
-            result = chebyshev_cos_impl(norm_a);
-            break;
-        case 3:
-        case 4:
-            result = -chebyshev_sin_impl(norm_a);
-            break;
-        case 5:
-        case 6:
-            result = -chebyshev_cos_impl(norm_a);
-            break;
+    if (use_cos) {
+        result = chebyshev_cos_impl(norm_a); // Q0.63
+    } else {
+        result = chebyshev_sin_impl(norm_a); // Q0.63
     }
+    result = neg_result ? -result : result;
 
     // Round to Q31.32
     result += (INT64_C(1) << (CHEBYSHEV_FRAC_BITS - FIX64_FRAC_BITS - 1));
@@ -51,42 +45,36 @@ fix64_t fix64_sin(fix64_t angle) {
 }
 
 fix64_t fix64_cos(fix64_t angle) {
-    // Normalise so that 1.0 = pi/2 = 90deg
+    // Normalise so that 1.0 = pi/4 = 45deg
     int64_t angle_hi;
-    uint64_t angle_lo = fix64_impl_mul_i64_i128(angle.repr, CHEBYSHEV_2_PI, &angle_hi); // Q31.94
+    uint64_t angle_lo = fix64_impl_mul_i64_i128(angle.repr, CHEBYSHEV_4_PI, &angle_hi); // Q31.94
 
-    // Calculate octant, used to determine which chebyshev function to call
-    uint32_t hi_frac_bits = CHEBYSHEV_FRAC_BITS + FIX64_FRAC_BITS - 64; // frac bits in angle_hi
-    unsigned octant = (angle_hi >> (hi_frac_bits - 1)) & 0x7;
-    // Modulo pi/2 (90deg)
-    angle_hi &= (1ull << hi_frac_bits) - 1; // UQ0.94
+    // Modulo 8 (2pi, i.e. 360deg)
+    unsigned hi_frac_bits = CHEBYSHEV_FRAC_BITS + FIX64_FRAC_BITS - 64; // frac bits in angle_hi
+    angle_hi &= (1ull << (hi_frac_bits + 3)) - 1; // UQ3.94
 
-    // Normalise -pi/4..pi/4 (90deg) to range -1..1 with CHEBYSHEV_FRAC_BITS fractional bits
+    // Normalise to Q3.62, so angle is in the range [-pi/4, pi/4)
+    angle_lo =
+        ((uint64_t)angle_hi << (64 - FIX64_FRAC_BITS)) | (angle_lo >> FIX64_FRAC_BITS); // UQ3.62
+    // Constrain to range -1..1 (i.e. -pi/4..pi/4)
+    angle_lo =
+        ((angle_lo + CHEBYSHEV_ONE) & (CHEBYSHEV_TWO - 1)) - CHEBYSHEV_ONE; // Q2.62 as unsigned
+    // Avoid UB of unsigned -> signed conversion overflow
     int64_t norm_a =
-        (angle_hi << (64 - (FIX64_FRAC_BITS - 1))) | (angle_lo >> (FIX64_FRAC_BITS - 1)); // Q1.62
-    norm_a =
-        (((norm_a + (1ull << CHEBYSHEV_FRAC_BITS)) & ((2ull << CHEBYSHEV_FRAC_BITS) - 1)) -
-         (1ull << CHEBYSHEV_FRAC_BITS)); // Q1.62
+        (angle_lo > INT64_MAX) ? (int64_t)(angle_lo - INT64_MIN) + INT64_MIN : (int64_t)angle_lo;
+    // Which eighth of the unit circle the angle lies in
+    unsigned octant = (angle_hi >> hi_frac_bits) + 1; // 1-8
+
+    int neg_result = ((octant >> 1) ^ (octant >> 2)) & 1; // negate result for 2,3,4,5
+    int use_sin = (octant & 2) != 0; // use cos for 2,3,6,7
 
     int64_t result = 0;
-    switch (octant) {
-        case 7:
-        case 0:
-            result = chebyshev_cos_impl(norm_a);
-            break;
-        case 1:
-        case 2:
-            result = -chebyshev_sin_impl(norm_a);
-            break;
-        case 3:
-        case 4:
-            result = -chebyshev_cos_impl(norm_a);
-            break;
-        case 5:
-        case 6:
-            result = chebyshev_sin_impl(norm_a);
-            break;
+    if (use_sin) {
+        result = chebyshev_sin_impl(norm_a);
+    } else {
+        result = chebyshev_cos_impl(norm_a);
     }
+    result = neg_result ? -result : result;
 
     // Round to Q31.32
     result += (INT64_C(1) << (CHEBYSHEV_FRAC_BITS - FIX64_FRAC_BITS - 1));
@@ -96,46 +84,39 @@ fix64_t fix64_cos(fix64_t angle) {
 }
 
 fix64_t fix64_tan(fix64_t angle) {
-    // Normalise so that 1.0 = pi/2 = 90deg
+    // Normalise so that 1.0 = pi/4 = 45deg
     int64_t angle_hi;
-    uint64_t angle_lo = fix64_impl_mul_i64_i128(angle.repr, CHEBYSHEV_2_PI, &angle_hi); // Q31.94
+    uint64_t angle_lo = fix64_impl_mul_i64_i128(angle.repr, CHEBYSHEV_4_PI, &angle_hi); // Q31.94
 
-    // Calculate octant, used to determine which chebyshev function to call
-    uint32_t hi_frac_bits = CHEBYSHEV_FRAC_BITS + FIX64_FRAC_BITS - 64; // frac bits in angle_hi
-    unsigned octant = (angle_hi >> (hi_frac_bits - 1)) & 0x7;
-    // Modulo pi/2 (90deg)
-    angle_hi &= (1ull << hi_frac_bits) - 1; // UQ0.94
+    // Modulo 8 (2pi, i.e. 360deg)
+    unsigned hi_frac_bits = CHEBYSHEV_FRAC_BITS + FIX64_FRAC_BITS - 64; // frac bits in angle_hi
+    angle_hi &= (1ull << (hi_frac_bits + 3)) - 1; // UQ3.94
 
-    // Normalise -pi/4..pi/4 (90deg) to range -1..1 with CHEBYSHEV_FRAC_BITS fractional bits
+    // Normalise to Q3.62, so angle is in the range [-pi/4, pi/4)
+    angle_lo =
+        ((uint64_t)angle_hi << (64 - FIX64_FRAC_BITS)) | (angle_lo >> FIX64_FRAC_BITS); // UQ3.62
+    // Constrain to range -1..1 (i.e. -pi/4..pi/4)
+    angle_lo =
+        ((angle_lo + CHEBYSHEV_ONE) & (CHEBYSHEV_TWO - 1)) - CHEBYSHEV_ONE; // Q2.62 as unsigned
+    // Avoid UB of unsigned -> signed conversion overflow
     int64_t norm_a =
-        (angle_hi << (64 - (FIX64_FRAC_BITS - 1))) | (angle_lo >> (FIX64_FRAC_BITS - 1)); // Q1.62
-    norm_a =
-        (((norm_a + (1ull << CHEBYSHEV_FRAC_BITS)) & ((2ull << CHEBYSHEV_FRAC_BITS) - 1)) -
-         (1ull << CHEBYSHEV_FRAC_BITS)); // Q1.62
+        (angle_lo > INT64_MAX) ? (int64_t)(angle_lo - INT64_MIN) + INT64_MIN : (int64_t)angle_lo;
+    // Which eighth of the unit circle the angle lies in
+    unsigned octant = (angle_hi >> hi_frac_bits) + 1; // 1-8
+
+    int recip_result = (octant & 2) != 0; // 1/result for 2,3,6,7
 
     int64_t result = 0;
-    switch (octant) {
-        case 7:
-        case 0:
-        case 3:
-        case 4:
-            result = chebyshev_tan_impl(norm_a); // Q1.62
-            // Round to Q1.32
-            result += (1ull << (CHEBYSHEV_FRAC_BITS - FIX64_FRAC_BITS - 1));
-            result >>= (CHEBYSHEV_FRAC_BITS - FIX64_FRAC_BITS); // Q31.32
-            break;
-        case 1:
-        case 2:
-        case 5:
-        case 6:
-            result = chebyshev_tan_impl(-norm_a);
-            if (!result) {
-                return FIX64_MAX;
-            }
-            // tan(x) = 1/tan(pi/2-x)
-            result = fix64_impl_div_i128_i64_sat(
-                1ull << (CHEBYSHEV_FRAC_BITS + FIX64_FRAC_BITS - 64), 0, result);
-            break;
+    if (recip_result) {
+        result = chebyshev_tan_impl(-norm_a);
+        // tan(x) = 1/tan(pi/2-x)
+        result = fix64_impl_div_i128_i64_sat(
+            1ull << (CHEBYSHEV_FRAC_BITS + FIX64_FRAC_BITS - 64), 0, result);
+    } else {
+        result = chebyshev_tan_impl(norm_a); // Q1.62
+        // Round to Q1.32
+        result += (1ull << (CHEBYSHEV_FRAC_BITS - FIX64_FRAC_BITS - 1));
+        result >>= (CHEBYSHEV_FRAC_BITS - FIX64_FRAC_BITS); // Q31.32
     }
 
     return (fix64_t){ result };
